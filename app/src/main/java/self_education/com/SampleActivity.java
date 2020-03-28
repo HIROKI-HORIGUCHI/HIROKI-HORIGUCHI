a902df90c27dc4bb1981815ef5056bfd94372afb
HORIGUCHI HIROKI <hiroki8.lovefootball@gmail.com>
2020-03-28 17:56:19 +0900

    first commit

---
+++ app/src/main/java/self_education/com/SampleActivity.java
@@ -0,0 +1,14 @@
+package self_education.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
